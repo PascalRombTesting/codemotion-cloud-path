70eebf49b7e0bdb1098644d5dd00d30a959930af
PascalRomb <pasquale.rombola97@gmail.com>
2022-02-02 13:54:50 +0100

    initial commit

---
+++ src/main/java/com/rhombusthere/codemotioncloudpath/CodemotionCloudPathApplication.java
@@ -0,0 +1,13 @@
+package com.rhombusthere.codemotioncloudpath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodemotionCloudPathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodemotionCloudPathApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rhombusthere/codemotioncloudpath/CodemotionCloudPathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhombusthere.codemotioncloudpath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodemotionCloudPathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
